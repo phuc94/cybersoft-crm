86d7ebe6ec87e148cf06f5553f86e928adb3cab5
Nguyễn Phúc <trungphuc94@gmail.com>
2025-02-13 16:03:44 +0700

    add RoleEntity & update UserEntity

--- src/main/java/crm08/entity/UserEntity.java
+++ src/main/java/crm08/entity/UserEntity.java
@@ -3,7 +3,36 @@ package crm08.entity;
 public class UserEntity {
 	private int id;
 	private String email;
+	private String password;
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String avatar;
+	private String fullname;
+	private RoleEntity role;
 
+	public String getAvatar() {
+		return avatar;
+	}
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public RoleEntity getRole() {
+		return role;
+	}
+	public void setRole(RoleEntity role) {
+		this.role = role;
+	}
 	public int getId() {
 		return id;
 	}
