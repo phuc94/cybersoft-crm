6f0ed0adcd2681e2260b9662e2d40415bbd25f4b
Nguyễn Phúc <trungphuc94@gmail.com>
2025-02-13 16:42:05 +0700

    add basic authentication

--- src/main/java/crm08/controller/UserController.java
+++ src/main/java/crm08/controller/UserController.java
@@ -8,12 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "user", urlPatterns = {"/user", "/user-add"})
+@WebServlet(name = "user", urlPatterns = {"/user", "/user-add", "/user-details"})
 public class UserController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("login.html").forward(req, resp);
+		req.getRequestDispatcher("login.jsp").forward(req, resp);
 	}
 
 }
--- src/main/java/crm08/filter/AuthenticationFilter.java
+++ src/main/java/crm08/filter/AuthenticationFilter.java
@@ -1,6 +1,8 @@
 package crm08.filter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -13,20 +15,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebFilter(filterName = "authenFilter", urlPatterns = {
-	"/user-add",
-	"/user-details",
-	"/user-table",
-	"/role-add",
-	"/role-table",
-	"/task",
-	"/task-add",
-	"/profile",
-	"/profile-edit",
-	"/groupwork",
-	"/groupwork-add",
-	"/groupwork-details",
+	"/user*",
+	"/role*",
+	"/task*",
+	"/profile*",
+	"/groupwork*",
 })
 public class AuthenticationFilter implements Filter {
+	private static String ADMIN = "ROLE_ADMIN";
+	private static String MANAGER = "ROLE_MANAGER";
+	private static String USER = "ROLE_USER";
 
 	@Override
 	public void doFilter(ServletRequest sevlReq, ServletResponse sevlRes, FilterChain chain)
@@ -36,40 +34,101 @@ public class AuthenticationFilter implements Filter {
 		Cookie[] reqCookies = req.getCookies();
 		String roleName = "";
 		String path = req.getServletPath();
+		List<String> allowedRoles = new ArrayList<String>();
 		
 		if (reqCookies == null) {
 			req.getRequestDispatcher("login.jsp").forward(req, res);
-		}
-		
-		for (Cookie cookie : reqCookies) {
-			if (cookie.getName() == "role") {
-				roleName = cookie.getValue();
+		} else {
+			for (Cookie cookie : reqCookies) {
+				if (cookie.getName() == "role") {
+					roleName = cookie.getValue();
+				}
 			}
 		}
 		
-		if (roleName.equals("")) {
+		if (roleName.isEmpty()) {
 			req.getRequestDispatcher("login.jsp").forward(req, res);
 		}
 		
 		switch (path) {
 			case "user-add": {
-				if (roleName.equals("ROLE_ADMIN")) {
-					chain.doFilter(req, res);
-				} else {
-					req.getRequestDispatcher("login.jsp").forward(req, res);
-				}
+				allowedRoles.add(ADMIN);
+				checkRole(roleName, allowedRoles, chain, req, res);
 				break;
 			}
 			case "user-table": {
-				if (roleName.equals("ROLE_ADMIN")) {
-					chain.doFilter(req, res);
-				} else {
-					req.getRequestDispatcher("login.jsp").forward(req, res);
-				}
+				allowedRoles.add(ADMIN);
+				checkRole(roleName, allowedRoles, chain, req, res);
+				break;
+			}
+			case "user-details": {
+				allowedRoles.add(ADMIN);
+				checkRole(roleName, allowedRoles, chain, req, res);
+				break;
+			}
+			case "groupwork": {
+				allowedRoles.add(MANAGER);
+				checkRole(roleName, allowedRoles, chain, req, res);
+				break;
+			}
+			case "groupwork-add": {
+				allowedRoles.add(ADMIN);
+				checkRole(roleName, allowedRoles, chain, req, res);
+				break;
+			}
+			case "groupwork-details": {
+				allowedRoles.add(ADMIN);
+				checkRole(roleName, allowedRoles, chain, req, res);
+				break;
+			}
+			case "profile-edit": {
+				allowedRoles.add(ADMIN);
+				checkRole(roleName, allowedRoles, chain, req, res);
+				break;
+			}
+			case "profile": {
+				allowedRoles.add(ADMIN);
+				checkRole(roleName, allowedRoles, chain, req, res);
+				break;
+			}
+			case "task": {
+				allowedRoles.add(USER);
+				checkRole(roleName, allowedRoles, chain, req, res);
+				break;
+			}
+			case "task-add": {
+				allowedRoles.add(ADMIN);
+				checkRole(roleName, allowedRoles, chain, req, res);
+				break;
+			}
+			case "role": {
+				allowedRoles.add(ADMIN);
+				checkRole(roleName, allowedRoles, chain, req, res);
+				break;
+			}
+			case "role-add": {
+				allowedRoles.add(ADMIN);
+				checkRole(roleName, allowedRoles, chain, req, res);
+				break;
+			}
+			case "role-table": {
+				allowedRoles.add(ADMIN);
+				checkRole(roleName, allowedRoles, chain, req, res);
 				break;
 			}
 			default:
+				chain.doFilter(req, res);
 				break;
+		}
+	}
+	
+	private void checkRole(String role, List<String> allowedRole, FilterChain chain, HttpServletRequest req, HttpServletResponse res)
+			throws IOException, ServletException {
+//		chain.doFilter(req, res);
+		if (allowedRole.contains(role)) {
+			chain.doFilter(req, res);
+		} else {
+			req.getRequestDispatcher("login.jsp").forward(req, res);
 		}
 	}
 	
